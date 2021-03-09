b8abdb82f4e0701729c0a230294dbaea9e49dc33
Maaz Hussain <maazhussain2012@gmail.com>
2021-03-10 03:26:44 +0500

    Add files via upload

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
  
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "Ubuntu_restApi";
 $dbname = "reliefme_db";
 define('databaseName','reliefme_db'); 
 $connect =mysqli_connect($servername, $username, $password, $dbname);
