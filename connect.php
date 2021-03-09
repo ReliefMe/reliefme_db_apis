ae4baa1ee3f334bf842e5cd4acfbe7535350bb64
Maaz Hussain <maazhussain2012@gmail.com>
2021-03-10 03:27:04 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
  
 $servername = "localhost";
 $username = "root";
-$password = "Ubuntu_restApi";
+$password = "ubuntu_restApi";
 $dbname = "reliefme_db";
 define('databaseName','reliefme_db'); 
 $connect =mysqli_connect($servername, $username, $password, $dbname);
